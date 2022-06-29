0ffe1173c52ae109b395330ee26dbd8bacfa4787
David Kochanski <105320855+davefoxxo@users.noreply.github.com>
2022-06-29 19:49:24 -0400

    Array over fancy pokebase wrapper

--- cogs/Mon.py
+++ cogs/Mon.py
@@ -2,8 +2,8 @@ from util.BotUtils import Utils
 from util.BotUtils import nextcord
 import nextcord
 from nextcord.ext import commands
+from util.ListUtils import POKEMON
 import random
-import pokebase
 
 class Mon(commands.Cog):
     def __init__(self, client):
@@ -14,10 +14,9 @@ class Mon(commands.Cog):
         async with ctx.typing():
             mon = random.randint(1, 898)
 
-            pk = pokebase.pokemon(mon)
-            namesplit = pk.name.split("-")[0]
-            namespace = pk.name.replace("-", " ")
-            print(pk.name)
+            name = POKEMON[mon-1]
+            namesplit = name.split("-")[0]
+            namespace = name.replace("-", " ")
 
             embed = nextcord.Embed(title = "Guess that Mon!", colour = 0xff0000)
             embed.set_image(url = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/{}.png".format(mon))
@@ -28,14 +27,14 @@ class Mon(commands.Cog):
         while i < 63:
             message = await self.client.wait_for("message")
             msg = message.content.lower()
-            if (msg == pk.name or msg == namesplit or msg == namespace) and msg != "tapu" and msg != "mr" and msg != "mime":
+            if (msg == name or msg == namesplit or msg == namespace) and msg != "tapu" and msg != "mr" and msg != "mime":
                 await Utils.generic_embed(ctx, title = f"Correct! {namespace.title()}", desc = f"{message.author.name} got it!")
                 break
             elif msg == "d!idk":
                 await Utils.generic_embed(ctx, title = f"It's ||{namespace.title()}!||")
                 break
             elif msg == "d!hint":
-                await Utils.generic_embed(ctx, title = f"It starts with the letter '{pk.name[0]}'")
+                await Utils.generic_embed(ctx, title = f"It starts with the letter '{name[0]}'")
             elif msg == "d!len":
                 await Utils.generic_embed(ctx, title = f"It's name is {len(namesplit)} letters long")
             elif msg == "d!mon":
