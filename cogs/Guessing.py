37e82e913e7e20fc9b3c734961978e1ad1f5a263
David Kochanski <105320855+davefoxxo@users.noreply.github.com>
2022-07-15 22:04:59 -0400

    Update mon to be like flag

--- cogs/Guessing.py
+++ cogs/Guessing.py
@@ -32,28 +32,30 @@ class Guessing(commands.Cog):
         namesplit = name.split("-")[0]
         namespace = name.replace("-", " ")
 
-        url = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/{}.png".format(mon)
+        url = requests.get("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/{}.png".format(mon))
+        img = Image.open(BytesIO(url.content))
+        img.save(os.path.join(DIR, "cogs\\save\\mon.png"))
         print(name)
 
+        file = nextcord.File(os.path.join(DIR, "cogs\\save\\mon.png"))
         embed = nextcord.Embed(title = "Guess that Mon!", colour = 0xff0000)
         embed.set_image(url = url)
 
-        await ctx.send(embed = embed)
+        await ctx.send(embed = embed, file = file)
 
         await Utils.handle_guesses(ctx, self.client, "mon", [name, namesplit, namespace], ["tapu", "mr", "mime"])
 
     @commands.cooldown(1, 2, commands.BucketType.user)
     @commands.command()
     async def flag(self, ctx):
-
         flag = random.choice(COUNTRIES).lower()
         namefirst = flag.split(" ")[0]
         namespace = flag.replace(" ", "")
         print(flag)
 
-        flag_url = requests.get("https://countryflagsapi.com/png/{}".format(flag.replace(" ", "%20")))
-        flag_img = Image.open(BytesIO(flag_url.content))
-        flag_img.save(os.path.join(DIR, "cogs\\save\\NICE_TRY_LUCA.png"))
+        url = requests.get("https://countryflagsapi.com/png/{}".format(flag.replace(" ", "%20")))
+        img = Image.open(BytesIO(url.content))
+        img.save(os.path.join(DIR, "cogs\\save\\NICE_TRY_LUCA.png"))
 
         file = nextcord.File(os.path.join(DIR, "cogs\\save\\NICE_TRY_LUCA.png"))
         embed = nextcord.Embed(title = "Guess that flag!", colour = 0xff0000)
